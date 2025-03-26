2a2ca9a91518879baac45bcd40182f744f6d12f0
enddev6 <enddeveloper6@gmail.com>
2025-03-26 18:18:31 +0800

    Delete Final Project Submission directory

--- Final Project Submission/R/graph_table.R
+++
@@ -1,8 +0,0 @@
-#' Enrollment and Census Information
-#'
-#' @name graph_table-data
-#' @docType data
-#' @author Kendall Loh \email{kendall.loh@columbia.edu}
-#' @references \url{https://usedgov.github.io/api/crdc.html}
-#' @keywords datasets
-"graph_table"
--- Final Project Submission/R/population_data.R
+++
@@ -1,9 +0,0 @@
-#' Census Data
-#'
-#' @name population_data-data
-#' @docType data
-#' @author Kendall Loh \email{kendall.loh@columbia.edu}
-#' @references \url{https://usedgov.github.io/api/crdc.html}
-#' @keywords datasets
-#'
-"population_data"
